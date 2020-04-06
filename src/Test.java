b376d7899124a320029a2c2975caf8f6b27e9ef5
truenine <2394997288@qq.com>
2020-04-06 17:58:04 +0800

    提交一个 hello world

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * ²âÊÔÏòÔ¶¶ËÍÆËÍ
+ *
+ * @author TrueNine
+ * @version 1.0
+ * @time 2020/4/6
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
